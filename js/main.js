da5ad496f8b5143c3d9df468e5b8f148d3670e96
Vitalii-Plechak <vitalii.plechak@gmail.com>
2020-07-08 08:33:38 +0300

    add mail.php

--- js/main.js
+++ js/main.js
@@ -282,6 +282,28 @@ $(function(){
 
 });
 
+$(function(){
+
+  //E-mail Ajax Send
+  $("#form-reservation").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+
+});
+
+
 new WOW().init();
 
 $(function () {
